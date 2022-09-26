9dc88f3facf67a7c1bf41c6ab2f30c42e261259a
Ststone <chima.miro@gmail.com>
2022-09-26 12:34:54 +0800

    origin

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("tags").innerHTML='<object type="text/html" data="tags.html" class="tags"></object>';
+document.getElementById("footer").innerHTML='<object type="text/html" data="footer.html" class="footer"></object>';
+document.getElementById("right").innerHTML='<object type="text/html" data="main.html" class="main"></object>';
---
+++ tags.js
@@ -0,0 +1,3 @@
+function test(){
+    window.opener.href = "#test";
+}
